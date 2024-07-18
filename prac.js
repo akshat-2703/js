d1770ca200116cadc13e523c04c9ced46e43c85e
akshat-2703 <akshatbhatt40@gmail.com>
2024-07-18 10:02:52 +0530

    kjd

--- prac.js
+++ prac.js
@@ -4,7 +4,7 @@
     return str.split('').reverse().join('') ;
 }
 console.log(rev(str)) ;
-*/
+
 // JavaScript function to check if an object has a certain property.
 function hasProperty(obj,property){
     if(obj[property]){
@@ -20,3 +20,27 @@ const obj = {
 }
 console.log(hasProperty(obj,"a"));
 console.log(hasProperty(obj,"z"));
+
+//JavaScript function to find the maximum value in an array.
+const array = [1,2,3,4,5] ;
+let mini = -1e9 ;
+for (let index = 0; index < array.length-1; index++) {
+     if(array[index+1] > array[index]){
+        mini = array[index+1] ;
+     }
+}
+console.log(mini);
+
+//More precise 
+const array = [1,2,3,4,5] ;
+console.log(Math.max(...array));
+*/
+const str = "racecar" ;
+let n = str.length ;
+let i = 0 , j = n-1 ;
+while(i <= j){
+    if(str[i++] != str[j--]){
+        console.log("false");
+    }
+}
+console.log("true");
