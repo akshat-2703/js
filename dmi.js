5b10ef4ca562bf767bb23617bfba0a2890265506
akshat-2703 <akshatbhatt40@gmail.com>
2024-07-21 13:41:10 +0530

    one

--- dmi.js
+++ dmi.js
@@ -17,7 +17,7 @@ document.getElementById('google').addEventListener('click',function(e){
     e.stopPropagation() ;
     console.log("google clicked");
 },false)
-*/ 
+
 document.querySelector('#images').addEventListener('click',function(e){
    // console.log(e.target.parentNode);
     
@@ -31,6 +31,43 @@ document.querySelector('#images').addEventListener('click',function(e){
    //   removeIt.parentNode.removeChild(removeIt)
 
 },false )
+*/
+//Javascript - Synchronous,single thread each operation waits for the last one compete
+//blocing code vs non blocing code, Execution Context
 
 //type,timestamp,defaultPrevented,target,toelemet,srcelement,currenttarget,clientx,clienty,screenx 
 //attach event
+/*
+const sayHitesh = function() {
+    console.log("akshat");
+}
+const changeText = function(){
+   document.querySelector('h1').innerHTML = "best JS series"
+}
+
+const changeMe = setTimeout(changeText,2000)
+
+//clearTimeout(changeMe) 
+document.querySelector('#stop').addEventListener('click',function(){
+    clearTimeout(changeMe)
+    console.log("Stopped");
+},false) 
+
+ const sayDate = function(str){
+    console.log(str,Date.now());
+ }
+const intervalId =  setInterval(sayDate,1000,"hi") 
+
+clearInterval(intervalId)
+
+let intervalId ;
+document.querySelector('#start').addEventListener('click',function(){
+   intervalId = setInterval(function(){
+    console.log(Date()); 
+   },1000);
+   console.log(intervalId);
+},false) 
+document.querySelector('#stop').addEventListener('click',function(){
+  clearInterval(intervalId);
+},false) ;
+*/
