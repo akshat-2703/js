0e25c07b020da8c6f6a354064ce67316c4ade8a8
akshat-2703 <akshatbhatt40@gmail.com>
2024-07-21 21:23:16 +0530

    th

---
+++ oop.js
@@ -0,0 +1,42 @@
+/*const user = {
+    username : "hitesh",
+    loginCount : 8,
+    signedIn : true,
+
+    getUserDetails : function(){
+      //     console.log(`Username : ${this.username}`);
+      //  console.log(this);
+    }
+}
+
+//  console.log(user.username) ;
+ // console.log(user.getUserDetails()) ;
+// console.log(this);
+
+//const promise1 = new Promise()
+//const date = new Date()
+
+function User(username,loginCount,isLoggedIn){
+    this.username = username
+    this.loginCount = loginCount 
+    this.isLoggedIn = isLoggedIn 
+   
+ //   return this 
+}
+
+const userOne = new User("hitesh",12,true)
+const userTwo = new User("hema",11,false)
+
+console.log(userOne);
+console.log(userOne);
+*/
+
+function multipleBy5(num){
+    return num*5 ;
+}
+
+multipleBy5.power = 2 ;
+
+console.log(multipleBy5(5));
+console.log(multipleBy5.power);
+console.log(multipleBy5.prototype);
