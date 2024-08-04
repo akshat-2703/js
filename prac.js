d14a1f59b98c7b1596e761f9d75399658f8fa269
akshat-2703 <akshatbhatt40@gmail.com>
2024-08-04 22:25:53 +0530

    new

--- prac.js
+++ prac.js
@@ -1,4 +1,7 @@
 //reverse a string
+
+const { get } = require("mongoose");
+
 /*const str = "hello" ;
  function rev(str){
     return str.split('').reverse().join('') ;
@@ -152,4 +155,49 @@ let promise = getPromise();
     console.log("rejected",err);
     
  })
-    */
+    
+ function asyncFunc() {
+    return new  Promise((resolve,reject) => {
+   setTimeout(()=>{
+    console.log("some data1");
+    resolve("success") ;
+   },3000) ;
+    });
+ }
+ console.log(("fetching data"));
+ 
+ let p1 = asyncFunc() ;
+ p1.then((res)=>{
+    console.log((res)) ;
+    
+ })
+ */
+function getData(dataId,getNextData){
+    return new Promise((resolve,reject) =>{
+        setTimeout(()=>{
+            console.log("data",dataId);
+            resolve("success") ;
+            if(getNextData){
+                getNextData() ;
+            }
+        },2000) ;
+    });
+}
+//Promise Chain
+console.log("getting data1....");
+getData(1)
+.then((res) => {
+    console.log("getting data2...");
+  return getData(2) ;
+})
+.then((res) => {
+    console.log("getting data3...");
+   return getData(3) ;
+})
+.then((res) =>{
+    console.log("getting data4...");
+    return getData(4) ;
+})
+.then((res) => {
+    console.log(res) ;  
+})
