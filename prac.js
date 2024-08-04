5272a67b5cf216b9a38019c6dbb822b560ababd9
akshat-2703 <akshatbhatt40@gmail.com>
2024-08-04 22:58:31 +0530

    newi

--- prac.js
+++ prac.js
@@ -170,8 +170,8 @@ let promise = getPromise();
  p1.then((res)=>{
     console.log((res)) ;
     
- })
- */
+})
+ 
 function getData(dataId,getNextData){
     return new Promise((resolve,reject) =>{
         setTimeout(()=>{
@@ -201,3 +201,63 @@ getData(1)
 .then((res) => {
     console.log(res) ;  
 })
+
+//async function always return a promise 
+//await pauses the execution of its surrounding async function until the promise is settled
+async function hello() {
+     console.log("hello");    
+}
+//await only used inside async funct
+
+function api(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log("weather data");
+       resolve(200) ;
+        },2000) ;
+    }) ;
+}
+async function  getWeatherData() {
+     await api() ; //1st 
+     await api() ; //2nd 
+}
+
+function getData(dataId){
+    return new Promise((resolve,reject) =>{
+        setTimeout(()=>{
+   console.log("data",dataId);
+   resolve("success") ;
+        },2000);
+    }); 
+}
+async function getAlldata() {
+    console.log("getting all datas");
+   await getData(1) ;
+   console.log("getting data1...");
+   await getData(2); 
+   console.log("getting data2...");
+   await getData(3) ;
+   console.log("getting data1...");
+}
+getAlldata() ;
+//iife is a function that is called immediately as soon as it is defined
+//iife use kuki jese async function aur await k liye ek naya function banana pad rha aur fr call fr alg krna pad rha to there is more greater approach we use iife 
+*/
+function getData(dataId){
+    return new Promise((resolve,reject) =>{
+        setTimeout(()=>{
+   console.log("data",dataId);
+   resolve("success") ;
+        },2000);
+    }); 
+}
+
+(async function () {
+    console.log("getting all datas");
+   await getData(1) ;
+   console.log("getting data1...");
+   await getData(2); 
+   console.log("getting data2...");
+   await getData(3) ;
+   console.log("getting data1...");
+})()  ;
